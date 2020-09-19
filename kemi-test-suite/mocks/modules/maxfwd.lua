2c0109b3a142decbb361803ee526fc08b8717012
ovoshlook <ovoshlook@gmail.com>
2020-09-19 23:45:03 +0200

    fix maxfwd

--- kemi-test-suite/mocks/modules/maxfwd.lua
+++ kemi-test-suite/mocks/modules/maxfwd.lua
@@ -1,7 +1,7 @@
 return {
     process_maxfwd = function(limit)
         KAMAILIO_CRASH_CHECK(debug.getinfo(1),1,limit)
-        local f = testData.forwards or 4
+        local f = variables.forwards or 4
         if f > limit then
             return -1
         end
