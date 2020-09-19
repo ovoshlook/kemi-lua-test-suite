248e9f318a789b8ed66d0b3424b40eba507b05e9
ovoshlook <ovoshlook@gmail.com>
2020-09-19 21:42:03 +0200

    check all possbile paths

---
+++ examples/tests/nested.lua
@@ -0,0 +1,6 @@
+nested_module_mocking = {
+    description = "tests nested module replacement",
+    testdedModule = "nested/subnested/test.lua",
+    testedFunction = "testFunc",
+    testedResult = "original nested module test",
+}
